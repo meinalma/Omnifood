b0cb19415416309d345c55ceb000fe51af0ce833
Dalma <lipcsei.dalma@gmail.com>
2019-12-22 16:30:54 +0000

    worked on and improved media queries, added hamburger menu

--- script.js
+++ script.js
@@ -86,5 +86,17 @@ $(document).ready(function(){
       offset: '50%'
     });
 
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function() {
+      var nav = $('.js--main-nav');
+      var icon = $('.js--nav-icon ion-icon');
+      nav.slideToggle(200);
+      if($('.ion-navicon-round').attr('name') == 'menu') {
+        $('.ion-navicon-round').attr('name','close');
+      } else {
+        $('.ion-navicon-round').attr('name','menu');
+      }
+      });
+
 
 });
